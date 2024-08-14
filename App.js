cb4ad5523790fde633515363835f3df34426f33d
YashSaini15 <yashs3393@gmail.com>
2024-08-14 14:42:11 +0530

    Initial Entry

---
+++ App.js
@@ -0,0 +1,41 @@
+import {
+  SafeAreaView,
+  StyleSheet,
+  Platform,
+  StatusBar
+} from 'react-native';
+import React, { useState } from 'react';
+import { colors } from './src/utils/colors';
+import { Focus } from './src/features/Focus';
+import { Timer } from './src/features/Timer';
+import { FocusHistory } from './src/features/FocusHistory';
+export default function App() {
+  const [currentSubject, setCurrentSubject] = useState('test');
+  const [history, setHistory] = useState([]);
+  return (
+    <SafeAreaView style={styles.container}>
+      {!currentSubject ? (
+        <>
+          <Focus addSubject={setCurrentSubject} />
+          <FocusHistory history={history} />
+        </>
+      ) : (
+        <Timer
+          focusSubject={currentSubject}
+          onTimerEnd={(subject) => {
+            setHistory([...history, subject])
+          }}
+          clearSubject={() => setCurrentSubject(null)}
+        />
+      )}
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    backgroundColor: colors.darkBlue,
+  },
+});
---
+++ src/features/FocusHistory.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { colors } from '../utils/colors';
+import { fontSizes, spacing } from '../utils/sizes';
+
+export const FocusHistory = ({ history }) => {
+  if (!history || !history.length)
+    return (
+      <View style={{ padding: spacing.sm }}>
+        <Text style={styles.title}>We haven't focused on anything yet</Text>
+      </View>
+    );
+
+  const renderItem = ({ item }) => <Text style={styles.item}>- {item}</Text>;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Things we've focused on:</Text>
+      <FlatList data={history} renderItem={renderItem} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: spacing.md,
+    flex: 1,
+  },
+  item: {
+    fontSize: fontSizes.md,
+    color: colors.white,
+    paddingTop: spacing.sm,
+  },
+  title: {
+    color: colors.white,
+    fontSize: fontSizes.md,
+    fontWeight: 'bold',
+  },
+});
